a60c6b319662c7ed648a19f8d813910263a7ec95
randika-chathuranga <randikachathuranga219@gmail.com>
2021-11-07 11:40:44 +0530

    added all the files

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar style="padding: 0px 90px" flat app color="white" light>
+    <v-toolbar-title>Vuetify App</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-list class="d-flex align center">
+      <v-list-item v-for="menu in menus" :key="menu.title" :to="menu.route">
+        <v-list-item-title>{{ menu.title }}</v-list-item-title>
+      </v-list-item>
+      <v-btn>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn outlined color="success">Get Started</v-btn>
+    </v-list>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+
+  data: () => ({
+    menus: [
+      { title: "Home", route: "home" },
+      { title: "About Us", route: "about-us" },
+      { title: "Portfolio", route: "portfolio" },
+      { title: "Project", route: "project" },
+      { title: "Gallery", route: "gallery" },
+      { title: "Contact", route: "contact" },
+    ],
+  }),
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col lg="6">
+        <h2>Welcome to our vuetify app</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque veniam
+          minima delectus harum explicabo laboriosam et, facilis adipisci
+          exercitationem vero. Distinctio sapiente atque optio alias neque velit
+          et, tempora sequi.
+        </p>
+        <v-btn color="success" rounded>Get Started</v-btn>
+        <v-btn color="indigo" rounded class="white--text">
+          <v-icon>mdi-play-circle</v-icon>Watch Video
+        </v-btn>
+      </v-col>
+      <v-col lg="6"></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
